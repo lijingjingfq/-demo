7eb15ec24d4d99bc6b34568219e9cb3df8e47f45
lijingjingfq <apple@appledeiMac.local>
2015-08-13 17:28:53 +0800

    kfhdjs

--- 动画demo/ViewController.m
+++ 动画demo/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+
+    __weak IBOutlet UIButton *bnt;
+    IBOutlet UIView *backView;
+}
+- (IBAction)bntClicked:(UIButton *)sender;
 
 @end
 
@@ -18,10 +24,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)viewWillLayoutSubviews
+{
+    backView.frame=self.view.bounds;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)bntClicked:(UIButton *)sender {
+    [UIView transitionWithView:self.view duration:1.0 options:UIViewAnimationOptionTransitionCurlDown animations:^{
+        [self.view addSubview:backView];
+    } completion:^(BOOL finished) {
+        [bnt removeFromSuperview];
+    }];
+}
 @end
